c443b8c58ffb8556c9a733c6dbde1f27bc505c9f
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-03-01 20:52:16 +0300

    Update База фактов и построение предикатов.pl номер 3

--- База фактов и построение предикатов.pl
+++ База фактов и построение предикатов.pl
@@ -86,3 +86,47 @@ husband(X,Y):-parent(X,C),parent(Y,C),woman(Y),man(X).
 
 % husband(+X) - shows husband od X
 husband(X):-husband(Y,X),write(Y).
+
+
+%№3
+
+%ONLY USING FACTS
+
+%grand_da(+X,+Y) - checks if X is granddaughter of Y
+grand_da(X,Y):-woman(X), parent(M,X),parent(Y,M).
+
+%grand_dats(+X) - shows all granddaughters of X
+grand_dats(X):-parent(X,C),parent(C,G),woman(G),write(G),nl,fail.
+
+
+%grand_ma_and_da(+X,+Y) - checks if X and Y are grand ma and granddauter
+% or granddaughter and grandma
+grand_ma_and_da(X,Y):-woman(X),woman(Y),parent(X,M),parent(M,Y); woman(X),woman(Y),parent(Y,M),parent(M,X).
+
+
+%niece(+X,+Y) - checks if X is niece of Y
+niece(X,Y):-woman(X),parent(P,X),parent(G,P),parent(G,Y).
+
+
+%all_nieces(+X) - shows all nieces of X
+all_nieces(X):-parent(P,X),parent(P,S),parent(S,C),woman(C),not(parent(X,C)),woman(P),write(C),nl,fail.
+
+
+%USING ALREADY CREATED PREDICATES
+
+%grand_da_2(+X,+Y) - checks if X is granddaughter of Y
+grand_da_2(X,Y):- woman(X),mother(M,X),parent(Y,M);woman(X), mother(M,X), husband(H,M),parent(Y,H).
+
+%grand_dats_2(+X) - shows all granddaugters of X
+grand_dats_2(X):-grand_da(D,X),write(D),nl,fail.
+
+% grand_ma_and_da_2(+X,+Y) - checks if X and Y are grand ma and
+% granddauter or granddaughter and grandma
+grand_ma_and_da_2(X,Y):-grand_da_2(X,Y),woman(X),woman(Y);grand_da_2(Y,X),woman(Y),woman(X).
+
+%niece_2(+X,+Y) - checks if X is niece of Y
+niece_2(X,Y):-woman(X),parent(P,X),b_s(P,Y).
+
+%all_nieces_2(+X) - shows all nieces of X
+all_nieces_2(X):-niece_2(N,X),write(N),nl,fail.
+
