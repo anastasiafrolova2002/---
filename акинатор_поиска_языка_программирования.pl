3d5778ee39425b7ede63278070a721c2cdb37585
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-03-02 12:46:03 +0300

    Update and rename акинатор_без_доп_вопросов.pl to акинатор_поиска_языка_программирования.pl

--- акинатор_поиска_языка_программирования.pl
+++ акинатор_поиска_языка_программирования.pl
@@ -7,7 +7,8 @@ high(prolog,1).
 high(c,1).
 high(asm,0).
 high(js,1).
-high(go, 1).
+high(piet, 1).
+high(go,1).
 
 decl(ruby,0).
 decl(c_sharp,0).
@@ -18,7 +19,8 @@ decl(prolog,1).
 decl(c,0).
 decl(asm,0).
 decl(js,1).
-decl(go, 0).
+decl(piet, 1).
+decl(go,0).
 
 interpret(ruby,1).
 interpret(python,1).
@@ -29,7 +31,8 @@ interpret(c_plu_plus,0).
 interpret(c,0).
 interpret(asm,0).
 interpret(js,1).
-interpret(go, 0).
+interpret(piet, 1).
+interpret(go,0).
 
 oop(ruby,3).
 oop(c_sharp,3).
@@ -40,7 +43,8 @@ oop(prolog,1).
 oop(c,0).
 oop(asm,0).
 oop(js,2).
-oop(go, 1).
+oop(piet, 1).
+oop(go,1).
 
 cross(ruby,1).
 cross(python,1).
@@ -51,7 +55,8 @@ cross(asm,1).
 cross(c_sharp,0).
 cross(f_sharp,0).
 cross(js, 1).
-cross(go, 1).
+cross(piet, 0).
+cross(go,1).
 
 visual(c_sharp,3).
 visual(ruby,2).
@@ -62,8 +67,8 @@ visual(prolog,1).
 visual(c,0).
 visual(asm,0).
 visual(js,2).
-visual(go, 1).
-
+visual(piet, 0).
+visual(go,1).
 
 mobile(c_sharp,0).
 mobile(ruby,0).
@@ -74,7 +79,21 @@ mobile(prolog,0).
 mobile(c,0).
 mobile(asm,0).
 mobile(js,1).
-mobile(go, 1).
+mobile(piet, 0).
+mobile(go,1).
+
+eso(c_sharp,0).
+eso(ruby,0).
+eso(python,0).
+eso(c_plu_plus,0).
+eso(f_sharp,0).
+eso(prolog,0).
+eso(c,0).
+eso(asm,0).
+eso(js,0).
+eso(piet, 1).
+eso(go,0).
+
 
 
 question1(X1):-	write("Is your language high level?"),nl,
@@ -116,11 +135,16 @@ question7(X7):-	write("Is your language for mobile phones?"),nl,
 				write("0. NO"),nl,
 				read(X7).
 
+question8(X8):-	write("Is it this Esoteric programming language?"),nl,
+				write("1. Yes"),nl,
+				write("0. NO"),nl,
+				read(X8).
+
 
 
 
 pr:-	question1(X1),question2(X2),question3(X3),question4(X4),
 		question5(X5),question6(X6),question7(X7),question8(X8),
 		high(X,X1),decl(X,X2),interpret(X,X3),oop(X,X4),
-		cross(X,X5),visual(X,X6),mobile(X,X7),
+		cross(X,X5),visual(X,X6),mobile(X,X7),eso(X,X8),
 		write(X).
