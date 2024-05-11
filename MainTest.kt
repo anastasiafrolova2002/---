6e213aaf01b859b17b7073a62cc63d5f420df0cb
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-05-11 07:39:46 +0300

    LR4 EX1 unit tests for funcs
    Tests for: max3, factup, fuctdown, calc, sumd, muld, maxd, mind

---
+++ MainTest.kt
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    fun max3() {
+        val main = Main()
+        val expected = 19
+        assertEquals(expected, main.max3(13,19,1))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun factup() {
+        val main = Main()
+        val expected = 24
+        assertEquals(expected, main.factup(4))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun factdown() {
+        val main = Main()
+        val expected = 120
+        assertEquals(expected, main.factdown(5))
+    }
+
+
+    @org.junit.jupiter.api.Test
+    fun calc() {
+        val main = Main()
+        val expected = 20
+        assertEquals(expected, main.calc(true)(5555))
+    }
+
+
+    @org.junit.jupiter.api.Test
+    fun sumd() {
+        val main = Main()
+        val expected = 20
+        assertEquals(expected, main.sumd(5555))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun muld() {
+        val main = Main()
+        val expected = 0
+        assertEquals(expected, main.muld(150))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun maxd() {
+        val main = Main()
+        val expected = 8
+        assertEquals(expected, main.maxd(103845))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun mind() {
+        val main = Main()
+        val expected = 2
+        assertEquals(expected, main.mind(998765325))
+    }
+}
